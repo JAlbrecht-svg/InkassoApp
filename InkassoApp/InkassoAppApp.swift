78807bb70a5c810c44c9f4641d5759d0985be9e0
Jannick Siewert-Albrecht <jannick.siewert-albrecht@outlook.com>
2025-04-19 16:09:00 +0200

    Initial Commit

---
+++ InkassoApp/InkassoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkassoAppApp.swift
+//  InkassoApp
+//
+//  Created by Jannick Niwat Siewert-Albrecht on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct InkassoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InkassoAppTests/InkassoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  InkassoAppTests.swift
+//  InkassoAppTests
+//
+//  Created by Jannick Niwat Siewert-Albrecht on 4/19/25.
+//
+
+import Testing
+@testable import InkassoApp
+
+struct InkassoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
